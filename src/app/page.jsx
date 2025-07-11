6f2ff3352d063660a9d709a5082e42f8a4d304b6
Muhammad Rohan <164128040+mianrohan623@users.noreply.github.com>
2025-07-11 23:58:05 +0500

    landing page completed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,31 @@
+import Header from "@/components/Header";
+
+import Footer from "@/components/Footer";
+
+import Hero from "@/components/ui/home/Hero";
+
+import Features from "@/components/ui/home/Features";
+
+import CallToAction from "@/components/ui/home/CallToAction";
+
+import HowItWorks from "@/components/ui/home/HowItWorks";
+
+import UpcomingTournaments from "@/components/ui/home/UpcomingTournaments";
+
+import FAQ from "@/components/ui/home/FAQ";
+
 export default function Home() {
   return (
-    <div className="bg-red-400 p-5">
-      <div className="bg-brand p-5">
-        <p className="bg-brand-light">Hello Word</p>
-        <p className="bg-brand-dark">Hello Word</p>
-      </div>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <Features />
+      <CallToAction />
+      <HowItWorks />
+      <UpcomingTournaments />
+      <FAQ/>
+      <Footer />
+
+    </>
   );
 }
