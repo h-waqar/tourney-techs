2654bfc07b584de8f3ff94da4d5167589bb2fb3d
hwaqar <hamzawaqar556@gmail.com>
2025-07-29 23:55:39 +0500

    Solve merge issues

--- src/app/api/tournaments/[id]/route.js
+++ src/app/api/tournaments/[id]/route.js
@@ -85,50 +85,6 @@ export const GET = asyncHandler(async (req, context) => {
   return Response.json(tournament);
 });
 
-<<<<<<< Updated upstream
-=======
-// PATCH /api/tournaments/[id]
-export const PATCH = asyncHandler(async (req, context) => {
-  const authUser = await requireAuth(req);
-  const { id } = await context.params;
-
-  await requireTournamentStaff(id, authUser._id, ["owner", "organizer"]);
-
-  const data = await req.json();
-
-  const allowedFields = [
-    "name",
-    "description",
-    "bannerUrl",
-    "location",
-    "startDate",
-    "endDate",
-    "games",
-    "isPublic",
-    "status",
-  ];
-
-  const update = {};
-  for (const key of allowedFields) {
-    if (data[key] !== undefined) update[key] = data[key];
-  }
-
-  if (Object.keys(update).length === 0) {
-    throw new ApiError(400, "No valid fields provided for update");
-  }
-
-  const tournament = await Tournament.findByIdAndUpdate(
-    id,
-    { $set: update },
-    { new: true }
-  );
-
-  if (!tournament) throw new ApiError(404, "Tournament not found");
-
-  return Response.json({ message: "Tournament updated", tournament });
-});
-
->>>>>>> Stashed changes
 // DELETE /api/tournaments/[id]
 export const DELETE = asyncHandler(async (req, context) => {
   const authUser = await requireAuth(req);
